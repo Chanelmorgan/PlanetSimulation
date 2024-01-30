c70caa578b2868ed7c5f1071b2aeb56c5619311d
Chanel Morgan <chanelmorgan05@icloud.com>
2024-01-30 20:58:28 +0000

    Initialised the venus
    TO-DO: initialising planets with the real values

--- tutorial.py
+++ tutorial.py
@@ -65,8 +65,9 @@ def main():
     earth = Planet(-1 * Planet.AU, 0, 16, BLUE, 5.9742 * 10**24)
     mars = Planet(-1.524 * Planet.AU, 0, 12, RED, 6.39 * 10**23)
     mercury = Planet(0.387 * Planet.AU, 0, DARK_GREY, 0.33 * 10**24)
+    venus = Planet(0.723 * Planet.AU, 0, 14, WHITE, 4.8685 * 10**24)
 
-    planets = [sun, earth, mars, mercury]
+    planets = [sun, earth, mars, mercury,venus]
 
     while run:
         # Maximum of 60 fps
